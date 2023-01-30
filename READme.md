9b92f78e9c9f0835f5d9b1b4cd7372751d6e0418
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-01-30 12:12:06 -0300

    Projeto final numero sorteado

---
+++ READme.md
@@ -0,0 +1,19 @@
+## Projeto Número Sorteado
+
+O projeto foi feito com o professor Guilherme Lira da Alura.
+
+O curso feito é o "JavaScript: validações e reconhecimento de voz".
+
+Abordagens: Math Random, Web Speech API entre outros.
+
+Mais sobre o Web Speech: https://wicg.github.io/speech-api/#speechreco-section ou site da MDN.
+
+A aplicação foi aplicada conforme a autora achou melhor.
+
+Mudanças:
+    - Troca do background, font-size e color;
+    - Adicionei o h2 para habilitar o microfone com a cor vermelha e em negrito;
+    - Organização em pastas do projeto;
+    - Icon no site pego no favicon.ico.
+
+-> Curso finalizado em 30/01/2022.
---
+++ assets/app/validacao.js
@@ -0,0 +1,62 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER" || "FIM DE JOGO") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
